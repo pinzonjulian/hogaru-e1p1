004ee194251b259071a518fb7233c524ce5758cd
pinzonjulian <julianpinzoneslava@gmail.com>
2016-07-28 21:51:50 -0500

    Initialize repository. Add Static pages controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def ruby
+  end
+
+  def git
+  end
+end
---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+  end
+
+  test "should get ruby" do
+    get static_pages_ruby_url
+    assert_response :success
+  end
+
+  test "should get git" do
+    get static_pages_git_url
+    assert_response :success
+  end
+
+end
