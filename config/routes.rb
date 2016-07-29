9947bcc5b8c8ed26e80ae6f91b5b0ca67f388ae2
pinzonjulian <julianpinzoneslava@gmail.com>
2016-07-28 22:12:02 -0500

    Configure root to home

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'static_pages/home'
-
   get 'static_pages/ruby'
-
   get 'static_pages/git'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
